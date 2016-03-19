4d0dcac4a2bb7edc3c3c8c0f3fd2408cdcab092c
Dohoon Kim <dohoonk.smg@gmail.com>
2016-03-19 13:06:40 -0700

    add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+            format:  /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :password, presence: true, length: { minimum: 5 }
 end
