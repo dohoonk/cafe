cced3092f65fecbf9264eb4231c5a1a5c7fc9cb7
Dohoon Kim <dohoonk.smg@gmail.com>
2016-04-02 14:23:18 -0700

    Work on layout of Bean model

--- app/uploaders/beanimg_uploader.rb
+++ app/uploaders/beanimg_uploader.rb
@@ -25,23 +25,31 @@ class BeanimgUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  # process :scale => [300, 200]
   #
-  # def scale(width, height)
-  #   # do something
+  # def scale
+  #   process :resize_to_fit => [200, 300]
   # end
 
   # Create different versions of your uploaded files:
-  process :resize_to_fit => [400, 400]
+  # process :resize_to_fit => [300, 200]
   version :thumb do
-    process :resize_to_fit => [230, 250]
+    process :resize_to_fit => [300, -1]
+  end
+
+  version :small do
+    process :resize_to_fit => [300, 200]
+  end
+
+  version :large do
+    process :resize_to_fit => [300, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
